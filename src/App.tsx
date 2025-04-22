3bb3e4599a2d03f8448a96cb98a5332cf1f4bffe
aasiflm10 <aa6125405@gmail.com>
2025-04-22 11:26:14 +0530

    material ui added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div className="bg-sky-400">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Clinical Landscape Analysis</h1>
     </div>
   );
 }
